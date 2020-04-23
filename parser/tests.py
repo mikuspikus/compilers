833d8c14c923639a7738dc40302984e684dec997
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-23 22:10:52 +0500

    Added recursive descent parser

--- parser/tests.py
+++ parser/tests.py
@@ -1,14 +1,32 @@
 from unittest import TestCase
-from parser import RDParser
+from parser.parser import RDParser, ParseError
 
 import os
 
 class Test(TestCase):
 
     def test_good(self):
-        filename = 'tests/good.txt'
+        filename = 'parser/tests/good.txt'
         with open(filename) as f:
             s = f.read()
 
         parser = RDParser(s)
-        parser.run()
+        result, tuple_ = parser.run()
+
+        self.assertEqual(result, True)
+
+    def test_fst_bad(self):
+        filename = 'parser/tests/fst_bad.txt'
+        with open(filename) as f:
+            s = f.read()
+
+        parser = RDParser(s)
+        self.assertRaises(ParseError, parser.run)
+
+    def test_snd_bad(self):
+        filename = 'parser/tests/snd_bad.txt'
+        with open(filename) as f:
+            s = f.read()
+
+        parser = RDParser(s)
+        self.assertRaises(ParseError, parser.run)
